9091b9c8a448eafd7f49f64d99d812828327f70a
Allison Casey <allison.casey@reifyhealth.com>
2023-08-10 18:45:36 +0000

    fix: set force_ssl to compile time config
    closes #1

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,8 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :safe_bike_routes, SafeBikeRoutesWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: SafeBikeRoutes.Finch
